7659f0ed85c3446918c3a9b49c15df46a5a6764b
Priyadharshini <priyathiru333@gmail.com>
2026-04-26 22:34:28 +0530

    Update CorsConfig.java

--- src/main/java/com/priya/depra/Config/CorsConfig.java
+++ src/main/java/com/priya/depra/Config/CorsConfig.java
@@ -1,28 +1,24 @@
 package com.priya.depra.Config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:3000",
-                                "https://ecommerce-fullstack-application-depra-production.up.railway.app"
-                        )
-                        .allowedMethods("*")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
