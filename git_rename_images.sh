939e6cfead6d755bd45b525e871366e2dc4eb9f7
Priyadeepa3 <priyathiru333@gmail.com>
2026-05-18 13:39:21 +0530

    fix: normalize all image filenames to lowercase with underscores

---
+++ git_rename_images.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+# Run this script from your project root to fix filename casing in Git
+# It uses a temp rename trick to force Git to track case-only renames on Windows/Mac
+
+cd src/main/resources/static/images
+
+git mv "Lavender_Floral_Printed_Kurti.jpeg" "_tmp_lavender_floral_printed_kurti.jpeg"
+git mv "_tmp_lavender_floral_printed_kurti.jpeg" "lavender_floral_printed_kurti.jpeg"
+
+git mv "Red_Kalamkari_Printed_Saree.jpeg" "_tmp_red_kalamkari_printed_saree.jpeg"
+git mv "_tmp_red_kalamkari_printed_saree.jpeg" "red_kalamkari_printed_saree.jpeg"
+
+git mv "aqua_blue_Lace_Party_Top.jpeg" "_tmp_aqua_blue_lace_party_top.jpeg"
+git mv "_tmp_aqua_blue_lace_party_top.jpeg" "aqua_blue_lace_party_top.jpeg"
+
+git mv "baby_pink_Cotton_Daily_Kurti.jpeg" "_tmp_baby_pink_cotton_daily_kurti.jpeg"
+git mv "_tmp_baby_pink_cotton_daily_kurti.jpeg" "baby_pink_cotton_daily_kurti.jpeg"
+
+git mv "black_Chanderi_Lightweight_Saree.jpeg" "_tmp_black_chanderi_lightweight_saree.jpeg"
+git mv "_tmp_black_chanderi_lightweight_saree.jpeg" "black_chanderi_lightweight_saree.jpeg"
+
+git mv "black_Cotton_Daily_Wear_Saree.jpeg" "_tmp_black_cotton_daily_wear_saree.jpeg"
+git mv "_tmp_black_cotton_daily_wear_saree.jpeg" "black_cotton_daily_wear_saree.jpeg"
+
+git mv "black_Kalamkari_Printed_Saree.jpeg" "_tmp_black_kalamkari_printed_saree.jpeg"
+git mv "_tmp_black_kalamkari_printed_saree.jpeg" "black_kalamkari_printed_saree.jpeg"
+
+git mv "black_Mirror_Work_Blouse.jpeg" "_tmp_black_mirror_work_blouse.jpeg"
+git mv "_tmp_black_mirror_work_blouse.jpeg" "black_mirror_work_blouse.jpeg"
+
+git mv "black_Red_Designer_Blouse.jpeg" "_tmp_black_red_designer_blouse.jpeg"
+git mv "_tmp_black_red_designer_blouse.jpeg" "black_red_designer_blouse.jpeg"
+
+git mv "black_Velvet_Party_Blouse.jpeg" "_tmp_black_velvet_party_blouse.jpeg"
+git mv "_tmp_black_velvet_party_blouse.jpeg" "black_velvet_party_blouse.jpeg"
+
+git mv "blue_Banarasi_Silk_Saree.jpeg" "_tmp_blue_banarasi_silk_saree.jpeg"
+git mv "_tmp_blue_banarasi_silk_saree.jpeg" "blue_banarasi_silk_saree.jpeg"
+
+git mv "blue_Floral_Printed_Kurti.jpeg" "_tmp_blue_floral_printed_kurti.jpeg"
+git mv "_tmp_blue_floral_printed_kurti.jpeg" "blue_floral_printed_kurti.jpeg"
+
+git mv "blue_Georgette_Designer_Saree.jpeg" "_tmp_blue_georgette_designer_saree.jpeg"
+git mv "_tmp_blue_georgette_designer_saree.jpeg" "blue_georgette_designer_saree.jpeg"
+
+git mv "blue_Golden_Embroidery_Blouse.jpeg" "_tmp_blue_golden_embroidery_blouse.jpeg"
+git mv "_tmp_blue_golden_embroidery_blouse.jpeg" "blue_golden_embroidery_blouse.jpeg"
+
+git mv "blue_Kanchipuram_Bridal_Silk_Saree.jpeg" "_tmp_blue_kanchipuram_bridal_silk_saree.jpeg"
+git mv "_tmp_blue_kanchipuram_bridal_silk_saree.jpeg" "blue_kanchipuram_bridal_silk_saree.jpeg"
+
+git mv "blue_Silk_Blouse_Piece.jpeg" "_tmp_blue_silk_blouse_piece.jpeg"
+git mv "_tmp_blue_silk_blouse_piece.jpeg" "blue_silk_blouse_piece.jpeg"
+
+git mv "burnt_orange_Evening_Party_Dress.jpeg" "_tmp_burnt_orange_evening_party_dress.jpeg"
+git mv "_tmp_burnt_orange_evening_party_dress.jpeg" "burnt_orange_evening_party_dress.jpeg"
+
+git mv "coffee_Sleeveless_Summer_Top.jpeg" "_tmp_coffee_sleeveless_summer_top.jpeg"
+git mv "_tmp_coffee_sleeveless_summer_top.jpeg" "coffee_sleeveless_summer_top.jpeg"
+
+git mv "cotton-blouse-piece.jpeg" "_tmp_cotton_blouse_piece.jpeg"
+git mv "_tmp_cotton_blouse_piece.jpeg" "cotton_blouse_piece.jpeg"
+
+git mv "daily-wear-cotton-kurti.jpeg" "_tmp_daily_wear_cotton_kurti.jpeg"
+git mv "_tmp_daily_wear_cotton_kurti.jpeg" "daily_wear_cotton_kurti.jpeg"
+
+git mv "electric_indig_Banarasi_Zari_Saree.jpeg" "_tmp_electric_indig_banarasi_zari_saree.jpeg"
+git mv "_tmp_electric_indig_banarasi_zari_saree.jpeg" "electric_indig_banarasi_zari_saree.jpeg"
+
+git mv "emerald_green_Anarkali_Festive_Kurti.jpeg" "_tmp_emerald_green_anarkali_festive_kurti.jpeg"
+git mv "_tmp_emerald_green_anarkali_festive_kurti.jpeg" "emerald_green_anarkali_festive_kurti.jpeg"
+
+git mv "fuchsia_Summer_Cotton_Dress.jpeg" "_tmp_fuchsia_summer_cotton_dress.jpeg"
+git mv "_tmp_fuchsia_summer_cotton_dress.jpeg" "fuchsia_summer_cotton_dress.jpeg"
+
+git mv "gold_Gold_Plated_Bangles.jpeg" "_tmp_gold_gold_plated_bangles.jpeg"
+git mv "_tmp_gold_gold_plated_bangles.jpeg" "gold_gold_plated_bangles.jpeg"
+
+git mv "gold_Gold_Plated_Bangles1.jpeg" "_tmp_gold_gold_plated_bangles1.jpeg"
+git mv "_tmp_gold_gold_plated_bangles1.jpeg" "gold_gold_plated_bangles1.jpeg"
+
+git mv "gold_Golden_Bridal_Blouse.jpeg" "_tmp_gold_golden_bridal_blouse.jpeg"
+git mv "_tmp_gold_golden_bridal_blouse.jpeg" "gold_golden_bridal_blouse.jpeg"
+
+git mv "gold_Golden_Bridal_Blouse1.jpeg" "_tmp_gold_golden_bridal_blouse1.jpeg"
+git mv "_tmp_gold_golden_bridal_blouse1.jpeg" "gold_golden_bridal_blouse1.jpeg"
+
+git mv "gold_Temple_Necklace_Set.jpeg" "_tmp_gold_temple_necklace_set.jpeg"
+git mv "_tmp_gold_temple_necklace_set.jpeg" "gold_temple_necklace_set.jpeg"
+
+git mv "gold_Temple_Necklace_Set1.jpeg" "_tmp_gold_temple_necklace_set1.jpeg"
+git mv "_tmp_gold_temple_necklace_set1.jpeg" "gold_temple_necklace_set1.jpeg"
+
+git mv "green_Mysore_Silk_Saree.jpeg" "_tmp_green_mysore_silk_saree.jpeg"
+git mv "_tmp_green_mysore_silk_saree.jpeg" "green_mysore_silk_saree.jpeg"
+
+git mv "green_Organza_Party_Saree.jpeg" "_tmp_green_organza_party_saree.jpeg"
+git mv "_tmp_green_organza_party_saree.jpeg" "green_organza_party_saree.jpeg"
+
+git mv "green_Printed_Blouse_Fabric.jpeg" "_tmp_green_printed_blouse_fabric.jpeg"
+git mv "_tmp_green_printed_blouse_fabric.jpeg" "green_printed_blouse_fabric.jpeg"
+
+git mv "green_Rayon_Straight_Kurti.jpeg" "_tmp_green_rayon_straight_kurti.jpeg"
+git mv "_tmp_green_rayon_straight_kurti.jpeg" "green_rayon_straight_kurti.jpeg"
+
+git mv "green_Silk_Blouse_Piece.jpeg" "_tmp_green_silk_blouse_piece.jpeg"
+git mv "_tmp_green_silk_blouse_piece.jpeg" "green_silk_blouse_piece.jpeg"
+
+git mv "hot_coral_Kanchipuram_Silk_Saree.jpeg" "_tmp_hot_coral_kanchipuram_silk_saree.jpeg"
+git mv "_tmp_hot_coral_kanchipuram_silk_saree.jpeg" "hot_coral_kanchipuram_silk_saree.jpeg"
+
+git mv "ice_blue_Cotton_Everyday_Blouse.jpeg" "_tmp_ice_blue_cotton_everyday_blouse.jpeg"
+git mv "_tmp_ice_blue_cotton_everyday_blouse.jpeg" "ice_blue_cotton_everyday_blouse.jpeg"
+
+git mv "maroon_Kasavu_Kerala_Saree.jpeg" "_tmp_maroon_kasavu_kerala_saree.jpeg"
+git mv "_tmp_maroon_kasavu_kerala_saree.jpeg" "maroon_kasavu_kerala_saree.jpeg"
+
+git mv "maroon_Printed_Blouse_Fabric.jpeg" "_tmp_maroon_printed_blouse_fabric.jpeg"
+git mv "_tmp_maroon_printed_blouse_fabric.jpeg" "maroon_printed_blouse_fabric.jpeg"
+
+git mv "maroon_Tant_Cotton_Saree.jpeg" "_tmp_maroon_tant_cotton_saree.jpeg"
+git mv "_tmp_maroon_tant_cotton_saree.jpeg" "maroon_tant_cotton_saree.jpeg"
+
+git mv "mustard_yellow_Bandhani_Festival_Saree.jpeg" "_tmp_mustard_yellow_bandhani_festival_saree.jpeg"
+git mv "_tmp_mustard_yellow_bandhani_festival_saree.jpeg" "mustard_yellow_bandhani_festival_saree.jpeg"
+
+git mv "olive_green_Cotton_Blouse_Piece.jpeg" "_tmp_olive_green_cotton_blouse_piece.jpeg"
+git mv "_tmp_olive_green_cotton_blouse_piece.jpeg" "olive_green_cotton_blouse_piece.jpeg"
+
+git mv "peacock_blue_Floral_Maxi_Dress.jpeg" "_tmp_peacock_blue_floral_maxi_dress.jpeg"
+git mv "_tmp_peacock_blue_floral_maxi_dress.jpeg" "peacock_blue_floral_maxi_dress.jpeg"
+
+git mv "pink_Cotton_Blouse_Piece.jpeg" "_tmp_pink_cotton_blouse_piece.jpeg"
+git mv "_tmp_pink_cotton_blouse_piece.jpeg" "pink_cotton_blouse_piece.jpeg"
+
+git mv "pink_Organza_Party_Saree.jpeg" "_tmp_pink_organza_party_saree.jpeg"
+git mv "_tmp_pink_organza_party_saree.jpeg" "pink_organza_party_saree.jpeg"
+
+git mv "pink_Soft_Mysore_silk_saree.jpeg" "_tmp_pink_soft_mysore_silk_saree.jpeg"
+git mv "_tmp_pink_soft_mysore_silk_saree.jpeg" "pink_soft_mysore_silk_saree.jpeg"
+
+git mv "red-designer-blouse.jpeg" "_tmp_red_designer_blouse.jpeg"
+git mv "_tmp_red_designer_blouse.jpeg" "red_designer_blouse.jpeg"
+
+git mv "red-velvet-blouse.jpeg" "_tmp_red_velvet_blouse.jpeg"
+git mv "_tmp_red_velvet_blouse.jpeg" "red_velvet_blouse.jpeg"
+
+git mv "red_Cotton_Daily_Kurti.jpeg" "_tmp_red_cotton_daily_kurti.jpeg"
+git mv "_tmp_red_cotton_daily_kurti.jpeg" "red_cotton_daily_kurti.jpeg"
+
+git mv "sliver_Oxidised_Silver_Earrings.jpeg" "_tmp_sliver_oxidised_silver_earrings.jpeg"
+git mv "_tmp_sliver_oxidised_silver_earrings.jpeg" "sliver_oxidised_silver_earrings.jpeg"
+
+git mv "turquoise_Silk_Blouse_Piece.jpeg" "_tmp_turquoise_silk_blouse_piece.jpeg"
+git mv "_tmp_turquoise_silk_blouse_piece.jpeg" "turquoise_silk_blouse_piece.jpeg"
+
+git mv "white_Anarkali_Festive_Kurti.jpeg" "_tmp_white_anarkali_festive_kurti.jpeg"
+git mv "_tmp_white_anarkali_festive_kurti.jpeg" "white_anarkali_festive_kurti.jpeg"
+
+git mv "white_Casual_Cotton_Top.jpeg" "_tmp_white_casual_cotton_top.jpeg"
+git mv "_tmp_white_casual_cotton_top.jpeg" "white_casual_cotton_top.jpeg"
+
+git mv "white_Oxidised_Silver_Earrings.jpeg" "_tmp_white_oxidised_silver_earrings.jpeg"
+git mv "_tmp_white_oxidised_silver_earrings.jpeg" "white_oxidised_silver_earrings.jpeg"
+
+git mv "white_Rayon_Straight_Kurti.jpeg" "_tmp_white_rayon_straight_kurti.jpeg"
+git mv "_tmp_white_rayon_straight_kurti.jpeg" "white_rayon_straight_kurti.jpeg"
+
+git mv "white_White_Cotton_Petticoat.jpeg" "_tmp_white_white_cotton_petticoat.jpeg"
+git mv "_tmp_white_white_cotton_petticoat.jpeg" "white_white_cotton_petticoat.jpeg"
+
+cd ../../../../..
+git add -A
+git commit -m "fix: normalize all image filenames to lowercase with underscores"
+git push
